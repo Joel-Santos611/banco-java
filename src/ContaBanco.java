884666f2a470a05d7ec831aa7d7b9798650b1288
Joel Dos Santos <jayds1711@gmail.com>
2024-05-20 20:49:00 +0100

    enviando atualização feitas da classe App para ContaTerminal, e os comentários

--- src/ContaBanco.java
+++ src/ContaBanco.java
@@ -6,7 +6,7 @@ public class ContaBanco {
 
 
 
- public void dadosDaConta(){
+ public void dadosDaConta(){//Esse metodo é responsavel por imprimir os dados da conta que serão inseridos pelo usuario via scanner
     System.out.println("Ola "+nomeCliente+
     ", obrigado por criar uma conta em nosso Banco, sua agencia é "+agencia+ 
     ", conta "+numero+" e seu saldo de "+saldo+" ja esta disponivel para saque!");
--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
-public class App {
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        //Criando o objeto conta e o Scanner
         ContaBanco conta=new ContaBanco();
         Scanner scanner=new Scanner(System.in);
 
+        //Obtendo dados pelo terminal e exibindo mensagens para o usuario
         System.out.println("Ola, vamos criar sua conta");
         System.out.println("Por favor, digite o numero da agencia:\n");
         conta.agencia=scanner.next();
@@ -14,10 +16,11 @@ public class App {
 
         System.out.println("Digite seu nome:\n");
         conta.nomeCliente=scanner.next();
-        
+
         System.out.println("Digite o valor que deseja depositar na conta:\n");
         conta.saldo=scanner.nextDouble();
 
+        //Imprimindo os dados no terminal
         conta.dadosDaConta();
 
     }
